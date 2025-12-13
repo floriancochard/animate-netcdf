421fc013fb3c9913437f80f5fb9b49e0b591e687
Florian <cochardflorian@gmail.com>
2025-12-13 10:21:15 +0100

    updated version

--- animate_netcdf/__init__.py
+++ animate_netcdf/__init__.py
@@ -10,7 +10,7 @@ This package provides functionality for:
 - File discovery and validation
 """
 
-__version__ = "1.7.1"
+__version__ = "1.7.2"
 __author__ = "Florian Cochard"
 __email__ = "florian@weatherwise.fr"
 
